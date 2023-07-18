ed43670a2a09e1539df5dd86c4b5892a83d4cdfd
Marco Junior MBELLA <mbellamarcojunior@gmail.com>
2023-07-18 11:20:20 +0200

    remove dead code

--- src/katta.test.ts
+++ src/katta.test.ts
@@ -6,59 +6,6 @@ import {
   IDiscountCalculator,
 } from "./types";
 
-// describe("function calculateBasketPrice", () => {
-//   it("Should return the total cost of the basket", () => {
-//     const basket: IBasketItem<IBook>[] = [
-//       {
-//         item: {
-//           name: "First book",
-//           price: 8,
-//         },
-//         quantity: 2,
-//       },
-//       {
-//         item: {
-//           name: "Second book",
-//           price: 8,
-//         },
-//         quantity: 2,
-//       },
-//       {
-//         item: {
-//           name: "third book",
-//           price: 8,
-//         },
-//         quantity: 2,
-//       },
-//       {
-//         item: {
-//           name: "fourt book",
-//           price: 8,
-//         },
-//         quantity: 1,
-//       },
-//       {
-//         item: {
-//           name: "five book",
-//           price: 8,
-//         },
-//         quantity: 1,
-//       },
-//     ];
-
-//     const discounts: Record<number, number> = {
-//       2: 0.05,
-//       3: 0.1,
-//       4: 0.2,
-//       5: 0.25,
-//     };
-
-//     const indicator = 4;
-
-//     expect(calculateBasketPrice(basket, discounts, indicator)).toEqual(51.2);
-//   });
-// });
-
 describe("DiscountCalculator", () => {
   const discounts: Record<number, number> = {
     2: 0.05,
